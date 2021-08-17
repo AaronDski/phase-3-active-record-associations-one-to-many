6b5d8dfda98b705d069467b04ed9bff46fe84575
AaronDski <aarondski123@gmail.com>
2021-08-17 11:20:22 -0600

    commit tree 5088b0be59beec6c8beedfda0dc91e148d7a7d7c

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ActiveRecord::Base
-  
+    has_many :reviews
+    # def reviews
+    #     Review.where(game_id: self.id)
+    # end
 end
+# game.reviews << Review.new(score: 3, comment: "terrible")
